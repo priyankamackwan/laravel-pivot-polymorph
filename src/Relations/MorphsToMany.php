9d2b318cc12e2a28ad701250feaeea1a25f07536
Priyanka <priyu08@gmail.com>
2023-03-16 19:36:22 +0530

    Make Reciprocal relationship

---
+++ src/Relations/MorphsToMany.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Pisochek\PivotPolymorph\Relations;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Support\Arr;
+
+class MorphsToMany extends MorphToMany
+{
+    /**
+     * The type of the related polymorphic relation.
+     *
+     * @var string
+     */
+    protected $relatedMorphType;
+
+    /**
+     * The class name of the related morph type constraint.
+     *
+     * @var string
+     */
+    protected $relatedMorphClass;
+
+    /**
+     * Create a new morph to many relationship instance.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Database\Eloquent\Model $parent
+     * @param string $name
+     * @param string $relatedName
+     * @param string $table
+     * @param string $type
+     * @param string $relatedType
+     * @param string $foreignPivotKey
+     * @param string $relatedPivotKey
+     * @param bool $parentKey
+     * @param $relatedKey
+     * @internal param string $relatedPivotKey
+     * @internal param string $foreignPivotKey
+     */
+    public function __construct(
+        Builder $query,
+        Model $parent,
+        $name,
+        $relatedName,
+        $table,
+        $foreignPivotKey,
+        $relatedPivotKey,
+        $type,
+        $relatedType,
+        $parentKey,
+        $relatedKey
+    ) {
+        $this->relatedMorphType = $relatedType;
+        $this->relatedMorphClass = $query->getModel()->getMorphClass();
+
+        parent::__construct(
+            $query,
+            $parent,
+            $name,
+            $table,
+            $foreignPivotKey,
+            $relatedPivotKey,
+            $parentKey,
+            $relatedKey,
+            null,
+            false
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function addWhereConstraints()
+    {
+        parent::addWhereConstraints();
+
+        $this->query->where($this->table . '.' . $this->relatedMorphType, $this->relatedMorphClass);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addEagerConstraints(array $models)
+    {
+        parent::addEagerConstraints($models);
+
+        $this->query->where($this->table . '.' . $this->relatedMorphType, $this->relatedMorphClass);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function baseAttachRecord($id, $timed)
+    {
+        return Arr::add(
+            parent::baseAttachRecord($id, $timed), $this->relatedMorphType, $this->relatedMorphClass
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRelationExistenceQuery(Builder $query, Builder $parentQuery, $columns = ['*'])
+    {
+        return parent::getRelationExistenceQuery($query, $parentQuery, $columns)
+                     ->where($this->table . '.' . $this->relatedMorphType, $this->relatedMorphClass);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function newPivotQuery()
+    {
+        return parent::newPivotQuery()->where($this->table . '.' . $this->relatedMorphType, $this->relatedMorphClass);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function newPivot(array $attributes = [], $exists = false)
+    {
+        $using = $this->using;
+
+        $pivot = $using ? $using::fromRawAttributes($this->parent, $attributes, $this->table, $exists)
+            : MorphsToManyPivot::fromAttributes($this->parent, $attributes, $this->table, $exists);
+
+        $pivot->setPivotKeys($this->foreignPivotKey, $this->relatedPivotKey)
+              ->setOtherMorphType($this->relatedMorphType)
+              ->setOtherMorphClass($this->relatedMorphClass)
+              ->setMorphType($this->morphType)
+              ->setMorphClass($this->morphClass);
+
+        return $pivot;
+    }
+
+    /**
+     * Get related the foreign key "type" name.
+     *
+     * @return string
+     */
+    public function getOtherMorphType()
+    {
+        return $this->relatedMorphType;
+    }
+
+    /**
+     * Get the class name of the related model.
+     *
+     * @return string
+     */
+    public function getOtherMorphClass()
+    {
+        return $this->relatedMorphClass;
+    }
+}
---
+++ src/Relations/MorphsToManyPivot.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Pisochek\PivotPolymorph\Relations;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphPivot;
+
+class MorphsToManyPivot extends MorphPivot
+{
+    /**
+     * The type of the polymorphic relation.
+     *
+     * Explicitly define this so it's not included in saved attributes.
+     *
+     * @var string
+     */
+    protected $relatedMorphType;
+
+    /**
+     * The value of the polymorphic relation.
+     *
+     * Explicitly define this so it's not included in saved attributes.
+     *
+     * @var string
+     */
+    protected $relatedMorphClass;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setKeysForSaveQuery($query)
+    {
+        $query->where($this->relatedMorphType, $this->relatedMorphClass);
+
+        return parent::setKeysForSaveQuery($query);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete()
+    {
+        $query = $this->getDeleteQuery();
+
+        $query->where($this->relatedMorphType, $this->relatedMorphClass);
+
+        return $query->delete();
+    }
+
+    /**
+     * Set the morph type for the related in pivot.
+     *
+     * @param  string $morphType
+     * @return $this
+     */
+    public function setOtherMorphType($morphType)
+    {
+        $this->relatedMorphType = $morphType;
+
+        return $this;
+    }
+
+    /**
+     * Set the morph class for the related in pivot.
+     *
+     * @param  string $morphClass
+     * @return \Illuminate\Database\Eloquent\Relations\MorphPivot
+     */
+    public function setOtherMorphClass($morphClass)
+    {
+        $this->relatedMorphClass = $morphClass;
+
+        return $this;
+    }
+}
